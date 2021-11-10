655de968a55df3352ad626c6d9dc0456d39e1af9
LamaTarek <88500194+LamaTarek@users.noreply.github.com>
2021-11-10 16:52:47 +0200

    Update and rename main.cpp to 583 - Prime Factors.cpp
    problem link : https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=524

--- 583 - Prime Factors.cpp
+++ 583 - Prime Factors.cpp
@@ -14,24 +14,6 @@ void init() {
     cin.tie(0);
     cin.sync_with_stdio(0);
 }
-bool is_prime(int n) {
-    if (n <= 1)return 0;
-    for (int i = 2; i <= n/i; i += 1 + (i & 1)) {
-        if (n % i == 0)return 0;
-    }
-    return 1;
-}
-int factorize_(int n) {
-    int cnt = 0;
-    for (int i = 2; i <= n / i; i += 1 + (i & 1)) {
-        if (n % i == 0) {
-            cnt++;
-            n = n % i;
-        }
-    }
-    if (n > 1)cnt++;
-    return cnt;
-}
 vector<ll> factorize(ll n) // sqrt(n)
 {
     vector<ll> divisors;
@@ -45,16 +27,6 @@ vector<ll> factorize(ll n) // sqrt(n)
     }
     if (n > 1) divisors.push_back(n);
     return divisors;
-}
-int gen_divisors(int n) {         //O(sqrt(n))
-    int cnt=0;
-    for (int i = 1; i <= n/i; ++i) {
-        if (n % i == 0) {
-            cnt++;
-            if (i * i != n)cnt++;
-        }
-    }
-    return cnt;
 }
 int main() {
     init();
